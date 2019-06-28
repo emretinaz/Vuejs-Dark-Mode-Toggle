760eb5c782beefee601adef7d9437704c0940fd7
Emre TINAZ <emretinaz@gmail.com>
2019-06-29 00:31:38 +0300

    smoothened

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app" :class="mode">
+    <TheHeader :mode="mode" @toggle="toggle"/>
+    <TheContent :mode="mode"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TheHeader from "@/components/TheHeader";
+import TheContent from "@/components/TheContent";
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  components: { TheHeader, TheContent },
+  data() {
+    return {
+      mode: "dark"
+    };
+  },
+  created() {
+    window.addEventListener("keyup", this.keyPress);
+  },
+  methods: {
+    keyPress(e) {
+      if (e.key === "t") {
+        this.toggle();
+      }
+    },
+
+    toggle() {
+      if (this.mode === "dark") {
+        this.mode = "light";
+      } else {
+        this.mode = "dark";
+      }
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Roboto", sans-serif;
+}
+.app {
+  width: 100vw;
+  min-height: 100vh;
+  background: #f3f3f3;
+  color: #152028;
+  transition: background 0.6s ease-in-out;
+}
+
+.dark {
+  background: #192734;
+  color: #fefefe;
 }
 </style>
---
+++ src/components/TheContent.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="content">
+    <h3>This is {{mode}} mode</h3>
+    <div class="image-grid">
+      <div class="col">
+        <img src="https://source.unsplash.com/random/1600x900">
+      </div>
+      <div class="col">
+        <img src="https://source.unsplash.com/1600x900">
+      </div>
+      <div class="col">
+        <img src="https://source.unsplash.com/featured/1600x900">
+      </div>
+    </div>
+    <p
+      class="para"
+    >Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ut, quasi eos error perspiciatis laudantium consectetur officiis? Obcaecati libero officia quas atque dolorem cupiditate repellat ut dicta ratione iusto sequi porro quos recusandae ipsam, eligendi perferendis, laborum eum expedita quo maiores tempore, doloribus saepe. Eligendi nesciunt animi delectus vitae eius?</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["mode"]
+};
+</script>
+
+<style>
+.content {
+  padding: 25px;
+}
+
+.content h3 {
+  font-size: 28px;
+  text-transform: uppercase;
+  font-weight: 300;
+  margin-bottom: 25px;
+}
+.image-grid {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 15px;
+}
+
+.image-grid .col img {
+  width: 100%;
+  height: auto;
+  border-radius: 10px;
+}
+
+.para {
+  width: 100%;
+  max-width: 600px;
+  margin: 25px auto 0px;
+}
+</style>
